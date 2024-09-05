39c2b4745a401a5d9c06180e389a4c1162b251af
Navneet170904 <navneet.yadav_cs.aiml21@gla.ac.in>
2024-09-05 19:34:04 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobquest-frontend-cijl.onrender.com',
+    origin:'*',
     credentials:true
 }
 
